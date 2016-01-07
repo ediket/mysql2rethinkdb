57605d639790f162682aea1d19fd32ab7af701f5
ironhee <iron@ediket.com>
2016-01-07 15:14:01 +0900

    refactoring getMysqlTables function

--- src/converters.js
+++ src/converters.js
@@ -17,13 +17,11 @@ export async function mysql2json({ connection, table }) {
   return fileName;
 }
 
-export function getMysqlTables(connection) {
-  return new Promise(resolve => {
-    connection.query('SHOW TABLES', (err, rows) => {
-      const tables = _.map(rows, row => _.values(row)[0]);
-      resolve(tables);
-    });
-  });
+export async function getMysqlTables(connection) {
+  connection = promisifyAll(connection);
+  const rows = await connection.queryAsync(`SHOW TABLES`);
+  const tables = _.map(rows, row => _.values(row)[0]);
+  return tables;
 }
 
 export async function json2rethinkdb({ fileName, database, table }) {
